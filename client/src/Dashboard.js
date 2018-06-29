95a10d5cdb1ef0a0b09b1568dca570f115fcccf5
Dameon1 <dameonmedoza@gmail.com>
2018-06-29 11:06:28 -0700

    added store and reducer

--- client/src/Dashboard.js
+++ client/src/Dashboard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Pet from './components/Pet'
 
 export default function Dashboard (props){
 
@@ -25,52 +25,66 @@ export default function Dashboard (props){
     return (
       <div className="pets">
         <div className="row">
-         <div className="col-6">
-     
-     
-     
-      <section className="pet">
-        <header className="cat">
-        <h2 className="pet-name">{catObj.name}</h2>
-        <img src={catObj.imageURL} 
-             alt={catObj.imageDescription} />
-        </header>
-        <main>
-          More about {catObj.name}
-          <dl>
-            <dt>Sex</dt><dd>{catObj.sex}</dd>
-            <dt>Age</dt><dd>{catObj.age}</dd>
-            <dt>Breed</dt><dd>{catObj.breed}</dd>
-            <dt>Story</dt><dd>{catObj.story}</dd>
-          </dl>
-          <button onClick={()=>console.log('working')}>Adopt</button>
-        </main>
-        </section>
-
-        
+          <div className="col-6">
+            <Pet
+              {...catObj}
+              onAdoptPet = {()=>console.log('not working')}
+              //onAdoptPet={() => this.props.dispatch(adoptCat())}
+            />
+          </div>
+          <div className="col-6">
+            <Pet
+              {...dogObj}
+              onAdoptPet = {()=>console.log('not working')}
+              //onAdoptPet={() => this.props.dispatch(adoptDog())}
+            />
+          </div>
         </div>
-
-        <div className="col-6">
-        <section className="pet">
-        <header className="dog">
-        <h2 className="pet-name">{dogObj.name}</h2>
-        <img src={dogObj.imageURL} 
-             alt={dogObj.imageDescription} />
-        </header>
-        <main>
-          More about {dogObj.name}
-          <dl>
-            <dt>Sex</dt><dd>{dogObj.sex}</dd>
-            <dt>Age</dt><dd>{dogObj.age}</dd>
-            <dt>Breed</dt><dd>{dogObj.breed}</dd>
-            <dt>Story</dt><dd>{dogObj.story}</dd>
-          </dl>
-          <button onClick={()=>console.log('working')}>Adopt</button>
-        </main>
-        </section>
-        </div>
-      </div>
-      </div>
+     </div>
     );
   }
 
+
+
+  // //{...this.props.catToAdopt}{...this.props.dogToAdopt}
+  // // <section className="pet">
+  //       <header className="cat">
+  //       <h2 className="pet-name">{catObj.name}</h2>
+  //       <img src={catObj.imageURL} 
+  //            alt={catObj.imageDescription} />
+  //       </header>
+  //       <main>
+  //         More about {catObj.name}
+  //         <dl>
+  //           <dt>Sex</dt><dd>{catObj.sex}</dd>
+  //           <dt>Age</dt><dd>{catObj.age}</dd>
+  //           <dt>Breed</dt><dd>{catObj.breed}</dd>
+  //           <dt>Story</dt><dd>{catObj.story}</dd>
+  //         </dl>
+  //         <button onClick={()=>console.log('working')}>Adopt</button>
+  //       </main>
+  //       </section>
+
+        
+  //       </div>
+
+  //       <div className="col-6">
+  //       <section className="pet">
+  //       <header className="dog">
+  //       <h2 className="pet-name">{dogObj.name}</h2>
+  //       <img src={dogObj.imageURL} 
+  //            alt={dogObj.imageDescription} />
+  //       </header>
+  //       <main>
+  //         More about {dogObj.name}
+  //         <dl>
+  //           <dt>Sex</dt><dd>{dogObj.sex}</dd>
+  //           <dt>Age</dt><dd>{dogObj.age}</dd>
+  //           <dt>Breed</dt><dd>{dogObj.breed}</dd>
+  //           <dt>Story</dt><dd>{dogObj.story}</dd>
+  //         </dl>
+  //         <button onClick={()=>console.log('working')}>Adopt</button>
+  //       </main>
+  //       </section>
+  //       </div>
+  //     </div>
---
+++ client/src/actions/dogs.js
@@ -0,0 +1,82 @@
+//import { REACT_APP_API_BASE_URL } from '../config';
+
+export const FETCH_DOG_REQUEST = 'FETCH_DOG_REQUEST';
+export const fetchDogRequest = () => ({
+  type: FETCH_DOG_REQUEST
+});
+
+export const FETCH_DOG_ERROR = 'FETCH_DOG_ERROR';
+export const fetchDogError = error => ({
+  error,
+  type: FETCH_DOG_ERROR
+});
+
+export const FETCH_DOG_SUCCESS = 'FETCH_DOG_SUCCESS';
+export const fetchDogSuccess = data => ({
+  data,
+  type: FETCH_DOG_SUCCESS
+});
+
+export const ADOPT_DOG_REQUEST = 'ADOPT_DOG_REQUEST';
+export const adoptDogRequest = () => ({
+  type: ADOPT_DOG_REQUEST
+});
+
+export const ADOPT_DOG_ERROR = 'ADOPT_DOG_ERROR';
+export const adoptDogError = () => ({
+  type: ADOPT_DOG_ERROR
+});
+
+export const ADOPT_DOG_SUCCESS = 'ADOPT_DOG_SUCCESS';
+export const adoptDogSuccess = () => ({
+  type: ADOPT_DOG_SUCCESS
+});
+
+
+/// --------------------------- GETTING A DOG ----------------------------- ///
+
+// export const fetchDog = () => dispatch => {
+//   console.log('Attempting to fetch a dog');
+//   dispatch(fetchDogRequest());
+
+//   fetch(`${REACT_APP_API_BASE_URL}/dog`)
+//     .then(res => {
+//       if (!res.ok) {
+//         console.log('error fetching the dog!');
+//         return Promise.reject(res.statusText);
+//       }
+//       return res.json();
+//     })
+//     .then(dog => {
+//       console.log('Got a dog! Dispatching fetchDogSuccess');
+//       dispatch(fetchDogSuccess(dog));
+//     })
+//     .then(error => fetchDogError(error));
+// };
+
+
+
+// /** --------------------------- DELETING A DOG ----------------------------- **/
+
+// export const adoptDog = () => dispatch => {
+//   console.log('adopt dog');
+//   dispatch(adoptDogRequest());
+
+//   fetch(`${REACT_APP_API_BASE_URL}/dog`, { method: 'DELETE' })
+//     .then(res => {
+//       if (!res.ok) {
+//         console.log('error adopting dog');
+//         return Promise.reject(res.statusText);
+//       }
+//       return res.statusText;
+//     })
+//     .then(dog => {
+//       console.log('dispatching adoptDogSuccess');
+//       dispatch(adoptDogSuccess());
+//     })
+//     .then(() => dispatch(fetchDog()))
+//     .then(error => adoptDogError(error));
+// };
+
+
+
